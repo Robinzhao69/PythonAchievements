ae87e5574727140f5b0cd5cc531e77b89b96a6c2
Robin <titanen.197@hotmail.com>
2020-09-16 14:31:54 +0200

    mooie poem

---
+++ PYTB1L2PoemPrinting/poem.py
@@ -0,0 +1,20 @@
+vers1 = "Hallo mijn naam is Robin Zhao en ik ben 16 jaar oud."
+vers2 = "Mijn hobby's zijn gamen en voetballen."
+vers3 = "Ik game best veel, maar dat is best, want als ik het goed op school kan doen dan word ik niet weggepest."
+vers4 = "ik ook voetbal, want dat vind ik leuk, maar al is er een ding waar ik niet tegen kan is dat ik die bal scheef wegtrap."
+vers5 = "ik woon in Hoorn, een mooie stad." 
+vers6 = "De stad heeft ongeveer 74.000 inwoners."
+vers7 = "ik zit bij het ma op school."
+vers8 = "ik moet op maandag en woensdag met de trein naar school."
+vers9 = "Als in eenmaal in amsterdam kom loop ik naar school."
+vers10 = "het is ongeveer 20 min lopen van station naar school."
+vers11 = "Misschien is het langzaam misschien is het snel, maar ik loop om mijn eigen tempo."
+vers12 = "ik zit op het ma, omdat ik het leuk vind om met computers te werken."
+vers13 = "Daarom hoop ik veel te leren op deze opleiding." 
+
+
+
+
+
+
+print(vers1,vers2,vers3,vers4,vers5,vers6,vers7,vers8,vers9,vers10,vers11,vers12,vers13)
