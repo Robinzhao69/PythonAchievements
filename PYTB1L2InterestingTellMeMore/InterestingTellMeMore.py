e8c4b9ef978890262738c11fc90d56dec9a75c79
Robin <titanen.197@hotmail.com>
2020-09-23 14:38:37 +0200

    klaar

---
+++ PYTB1L2InterestingTellMeMore/InterestingTellMeMore.py
@@ -0,0 +1,8 @@
+naam = input("Hallo wie ben jij?")
+print("Hallo " + naam)
+
+leeftijd = input("Hoe oud ben jij?")
+print("Oh zo je bent dus " + leeftijd + " jaar oud.")
+
+hobby = input("Wat zijn jou hobby's?")
+print("oh dus jou hobby's zijn " + hobby) 
