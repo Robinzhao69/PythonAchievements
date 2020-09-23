9fe3fe951b6ce96d4b635e019b286f16170db559
Robin <titanen.197@hotmail.com>
2020-09-23 15:11:00 +0200

    keuzes keuzes

---
+++ PYTB1L3DailyChoices/keuzes.py
@@ -0,0 +1,51 @@
+print("Als het regent, LOOP of REN je naar een schuilplek ?")
+keus1 = input()
+if keus1 == "LOOP" or keus1 == "loop":
+    print("Lekker lang in de regen.")
+elif keus1 == "ren" or keus1 == "REN":
+    print("Snel rennen!")
+else: 
+    print("ikke niet begrijpen.")
+
+
+
+print("Als je ijs hebt, EET of LIK je het ?")
+keus2 = input()
+if keus2 == "eet" or keus2 == "EET":
+    print("Auw doen jou tanden niet pijn ?")
+elif keus2 == "lik" or keus2 == "LIK":
+    print("Oei het ijsje smelt")
+else:
+    print("Dat was niet de vraag!")
+
+
+print("Iemand heeft een scheet gelaten. STILL zijn of SCHREEUWEN ?")
+keus3 = input()
+if keus3 == "still" or keus3 == "STILL":
+    print("Het stinkt een beetje.")
+elif keus3 == "schreeuwen" or keus3 == "SCHREEUWEN":
+    print("EEEUUUUWWW GADEVER DEMME. HET MEEURT.")
+else:
+    print("Oei succes.")
+
+
+print("Je bent uit huis getrapt. ga je weer TERUG naar binnen of ga je WEG ?")
+keus4 = input()
+if keus4 == "terug" or keus4 == "TERUG":
+    print("OH SHIT DE POLITIE KOMT ERAAN.")
+elif keus4 == "weg" or keus4 == "WEG":
+    print("Hup op naar een nieuw onderdak.")
+else:
+    print("Hoe krijg je het ook voor elkaar ?")
+
+
+print("Pizza of hamburger ?")
+keus5 = input()
+if keus5 == "pizza" or keus5 == "Pizza":
+    print("yummy")
+elif keus5 == "hamburger" or keus5 == "Hamburger":
+    print("yum yum")
+else:
+    print("advocado")
+
+
